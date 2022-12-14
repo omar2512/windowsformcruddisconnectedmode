7af41bf569e222face4fd44fe061de916903fbc5
omar2512 <113863685+omar2512@users.noreply.github.com>
2022-12-14 20:40:14 +0200

    Add files via upload

---
+++ itilabdisconnected/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace itilabdisconnected
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.sqlCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.tbnadd = new System.Windows.Forms.Button();
+            this.btndelet = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndisplay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(622, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 308);
+            this.listBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(245, 68);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(274, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(245, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(245, 205);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(274, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(245, 263);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(274, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=DESKTOP-PTQ4F3S\\SQL;Initial Catalog=cSharpLab;Integrated Security=Tru" +
+    "e";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(153, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Department";
+            // 
+            // tbnadd
+            // 
+            this.tbnadd.Location = new System.Drawing.Point(97, 353);
+            this.tbnadd.Name = "tbnadd";
+            this.tbnadd.Size = new System.Drawing.Size(75, 23);
+            this.tbnadd.TabIndex = 9;
+            this.tbnadd.Text = "Insert";
+            this.tbnadd.UseVisualStyleBackColor = true;
+            this.tbnadd.Click += new System.EventHandler(this.tbnadd_Click);
+            // 
+            // btndelet
+            // 
+            this.btndelet.Location = new System.Drawing.Point(226, 353);
+            this.btndelet.Name = "btndelet";
+            this.btndelet.Size = new System.Drawing.Size(75, 23);
+            this.btndelet.TabIndex = 10;
+            this.btndelet.Text = "Delete";
+            this.btndelet.UseVisualStyleBackColor = true;
+            this.btndelet.Click += new System.EventHandler(this.btndelet_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(355, 353);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(75, 23);
+            this.btnupdate.TabIndex = 11;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btndisplay
+            // 
+            this.btndisplay.Location = new System.Drawing.Point(490, 352);
+            this.btndisplay.Name = "btndisplay";
+            this.btndisplay.Size = new System.Drawing.Size(75, 23);
+            this.btndisplay.TabIndex = 12;
+            this.btndisplay.Text = "Display";
+            this.btndisplay.UseVisualStyleBackColor = true;
+            this.btndisplay.Click += new System.EventHandler(this.btndisplay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 493);
+            this.Controls.Add(this.btndisplay);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btndelet);
+            this.Controls.Add(this.tbnadd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Data.SqlClient.SqlCommand sqlCommand1;
+        private System.Windows.Forms.Button tbnadd;
+        private System.Windows.Forms.Button btndelet;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndisplay;
+    }
+}
+
---
+++ itilabdisconnected/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace itilabdisconnected
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-PTQ4F3S\SQL;Initial Catalog=cSharpLab;Integrated Security=True");
+        private void btndisplay_Click(object sender, EventArgs e)
+        {
+
+            SqlDataReader sdr;
+            try
+            {
+                if (listBox1.Items.Count > 0)
+                {
+                    listBox1.Items.Clear();
+                    comboBox1.Items.Clear();
+                }
+                sqlCommand1.CommandText = "Select id ,name ,depatment from person";
+                sqlCommand1.Connection = con;
+                con.Open();
+                sdr = sqlCommand1.ExecuteReader();
+                while (sdr.Read())
+                {
+                    string srg = (((int)sdr[0]).ToString()) + "\t" + sdr[1].ToString() + "\t" + sdr[2].ToString();
+                    listBox1.Items.Add(srg);
+                    comboBox1.Items.Add((int)sdr[0]);
+                }
+                sdr.Close();
+                con.Close();
+            }
+            catch (Exception ex){ MessageBox.Show(ex.ToString()); }
+        }
+
+        private void tbnadd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "insert into person values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "')";
+                Excutequery(str, "data has been added");
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void Excutequery(string st, string msg)
+        {
+            sqlCommand1.CommandText = st;
+            sqlCommand1.Connection = con;
+            con.Open();
+            int affectedrows= sqlCommand1.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show(affectedrows.ToString() + "rows changed" +msg);
+
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "update person set id='" + textBox1.Text + "',name='" + textBox2.Text + "',depatment ='" + textBox3.Text + "' where id='"+textBox1.Text+"'";
+                Excutequery(str,"data has been updated");
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlCommand1.CommandText = "select id ,name ,depatment from person where id='" + comboBox1.Text + "'";
+                sqlCommand1.Connection = con;
+                con.Open();
+                SqlDataReader sdr = sqlCommand1.ExecuteReader();
+                if (sdr.Read())
+                {
+                    textBox1.Text = ((int)sdr[0]).ToString();
+                    textBox2.Text = sdr[1].ToString();
+                    textBox3.Text = sdr[2].ToString();
+                }
+                sdr.Close();
+                con.Close();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btndelet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "delete from person where id='"+textBox1.Text+"'";
+                Excutequery(str, "data has been deleted");
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
